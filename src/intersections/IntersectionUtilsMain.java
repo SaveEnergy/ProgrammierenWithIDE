d9a35f5b2ffd407a0b5a5650499a73d0b0fae4d0
Sinan Ö <46645793+SaveEnergy@users.noreply.github.com>
2019-01-28 11:16:11 +0100

    Main Fix for int arrays

--- src/intersections/IntersectionUtilsMain.java
+++ src/intersections/IntersectionUtilsMain.java
@@ -3,8 +3,8 @@ package intersections;
 public class IntersectionUtilsMain {
 
   public static void main(String[] args) {
-    int array1[] = {1, 2, 3, 3, 4, 5};
-    int array2[] = {3, 5, 6};
+    int[] array1 = {1, 2, 3, 3, 4, 5};
+    int[] array2 = {3, 5, 6};
 
       for (int value : new IntersectionUtils().compare(array1, array2, "FULL")) {
         System.out.print(value + " ");
